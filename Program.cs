6478fd82e1d416140dcfb0acfb795efa9eedde11
Rys Vladislav <onesssse@gmail.com>
2018-07-03 19:32:22 +0300

    Clear console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
